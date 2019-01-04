1dad658a048ca55d27037d6fe95b0036a626e62d
Destrella3 <destrella385@gmail.com>
2019-01-04 13:13:38 -0500

    Data segues to detail view contoller

--- Elements/Elements/Cell/ElementViewCell.swift
+++ Elements/Elements/Cell/ElementViewCell.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ElementViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var elementImage: UIImageView!
     @IBOutlet weak var elementName: UILabel!
     @IBOutlet weak var atomicWeight: UILabel!
     
+    public func cellIcon() {
+        if let image = ImageHelper.shared.image(forKey: "http://www.theodoregray.com/periodictable/Tiles/ElementNumberWithThreeDigits/s7.JPG") {
+          elementImage.image = image
+        }
+    }
 }
--- Elements/Elements/Controllers/ElementDetailViewController.swift
+++ Elements/Elements/Controllers/ElementDetailViewController.swift
@@ -9,11 +9,30 @@
 import UIKit
 
 class ElementDetailViewController: UIViewController {
-
+    
+    public var element: Element!
+    
+    
+    @IBOutlet weak var elementImage: UIImageView!
+    @IBOutlet weak var elementSymbol: UILabel!
+    @IBOutlet weak var elementNumber: UILabel!
+    @IBOutlet weak var atomicWeight: UILabel!
+    @IBOutlet weak var meltingPoint: UILabel!
+    @IBOutlet weak var boilingPoint: UILabel!
+    @IBOutlet weak var discoveredBy: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        uploadData()
     }
     
+    private func uploadData() {
+        title = element.name
+        elementSymbol.text = "Symbol:  \(element.symbol)"
+        elementNumber.text = "Element Number:  \(element.number)"
+        atomicWeight.text = String(format: "Atomic Mass:  %.2f", element.atomicMass)
+        meltingPoint.text = String(format: "Melting Point:  %.2f", element.melt ?? 0.0)
+        boilingPoint.text = String(format: "Boiling Point:  %.2f", element.boil ?? 0.0)
+        discoveredBy.text = "Dicovered by:  \(element.discoveredBy ?? "Unkown")"
+    }
 }
