c6a46b29cd4a538154c3e695ec3332c58a37f8af
onielrosario <onielrosario@pursuit.org>
2019-01-04 17:25:50 -0500

    finished!!!! YAY!

--- Elements/Elements/controllers/FavoritesViewController.swift
+++ Elements/Elements/controllers/FavoritesViewController.swift
@@ -9,8 +9,45 @@
 import UIKit
 
 class FavoritesViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    private var favorites = [Favorite]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    var name = "Oniel"
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        title = "Favorites"
+        getFavorites()
+    }
+    
+    func getFavorites() {
+      guard let encodedName = name.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return }
+        elementAPIClient.getFavorites(name: encodedName) { (appError, favorites) in
+            if let appError = appError {
+                print(appError.errorMessage())
+            } else if let favorites = favorites {
+                self.favorites = favorites
+            }
+        }
+        
+    }
+}
+
+extension FavoritesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favoritesCell", for: indexPath)
+        let favorite = favorites[indexPath.row]
+        cell.textLabel?.text = favorite.elementName
+        cell.detailTextLabel?.text = "favorited by: \(favorite.favoritedBy)"
+        return cell
     }
 }
