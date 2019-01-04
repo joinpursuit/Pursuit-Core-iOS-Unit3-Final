a3c6a810ba83f75c6594bd1a0e0ad7a7661b7871
janezhu1618 <janezhu@pursuit.org>
2019-01-04 13:25:58 -0500

    4th commit - detail VC working with images and labels, autolayout also completed

--- Elements/Elements/Controllers/ElementDetailViewController.swift
+++ Elements/Elements/Controllers/ElementDetailViewController.swift
@@ -10,12 +10,35 @@ import UIKit
 
 class ElementDetailViewController: UIViewController {
     
+    @IBOutlet weak var elementSymbol: UILabel!
+    @IBOutlet weak var elementNumber: UILabel!
+    @IBOutlet weak var elementName: UILabel!
+    @IBOutlet weak var elementWeight: UILabel!
+    @IBOutlet weak var elementImage: UIImageView!
+    
     var element: Element!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = element.name
+        elementName.text = element.name
+        elementNumber.text = String(format: "%.2f", element.atomic_mass)
+        elementWeight.text = element.atomic_mass.description
+        elementSymbol.text = element.symbol
+        
+        let imageURL = "http://images-of-elements.com/\(element.name.lowercased()).jpg"
+        
+        if let url = URL.init(string: imageURL) {
+            do {
+                let data = try Data.init(contentsOf: url)
+                if let image = UIImage.init(data: data) {
+                    elementImage.image = image
+                }
+            } catch {
+                elementImage.image = UIImage(named: "placeholderImage")
+            }
+        }
+        
     }
-
 
 }
