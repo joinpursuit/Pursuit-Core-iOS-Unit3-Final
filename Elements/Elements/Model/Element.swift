53b8ee92e349629ae2c2e9d73125d3e9b018070f
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-12-19 10:48:53 -0500

    Created layout and Element Model

---
+++ Elements/Elements/Model/Element.swift
@@ -0,0 +1,21 @@
+//
+//  Element.swift
+//  Elements
+//
+//  Created by Amy Alsaydi on 12/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+struct Element {
+    let name: String
+    let discovered_by: String
+    let number: Int
+    let melt: Double
+    let symbol: String
+    let summary: String
+    let boil: Double
+    let atomic_mass: Double
+    let favoritedBy: String? 
+}
