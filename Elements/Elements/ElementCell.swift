c5449b61d98734c432f0bd6d387d2033478bc7b0
LubaKaper <liubovkaper@pursuit.com>
2019-12-19 17:24:23 -0500

    flickering

--- Elements/Elements/ElementCell.swift
+++ Elements/Elements/ElementCell.swift
@@ -17,29 +17,39 @@ class ElementCell: UITableViewCell {
     
     @IBOutlet weak var elementImage: UIImageView!
     
-    
+   private var urlString = ""
     
     func configureCell(for element: ElementInfo) {
         nameLabel.text = element.name
-        symbolLabel.text = "\(element.symbol)(\(element.number))  \(element.atomic_mass)"
+        symbolLabel.text = "\(element.symbol)(\(element.number))  \(element.atomic_mass ?? 0)"
         
         let elementId = String(format:"%03d", element.number)
         
         
         let imageURL = "http://www.theodoregray.com/periodictable/Tiles/\(elementId)/s7.JPG"
         
+        urlString = imageURL
         
-        elementImage.getImage(with: imageURL) { (result) in
+        elementImage.getImage(with: imageURL) { [weak self](result) in
             switch result {
-            case .failure(let appError):
-                print("error\(appError)")
+         case .failure:
+//                print("error\(appError)")
+                DispatchQueue.main.async {
+                  self?.elementImage.image = UIImage(systemName: "exclamationmark.triangle.fill")
+                }
                 
             case .success(let image):
                 DispatchQueue.main.async {
-                    self.elementImage.image = image
+                    if self?.urlString == imageURL {
+                    self?.elementImage.image = image
+                }
                 }
             }
         }
+    }
+    override func prepareForReuse() {
+      super.prepareForReuse()
+      elementImage.image = UIImage(systemName: "mic.fill")
     }
     
 }
--- Elements/NetworkServices/ElementAPIClient.swift
+++ Elements/NetworkServices/ElementAPIClient.swift
@@ -13,43 +13,43 @@ struct ElementAPIClient {
     static func fetchRemaining (completion: @escaping (Result<[ElementInfo], AppError>) ->()) {
         let remaininElementsURL = "https://5c1d79abbc26950013fbcaa9.mockapi.io/api/v1/elements_remaining"
         
-                guard let url2 = URL(string: remaininElementsURL ) else {
-                    completion(.failure(.badURL(remaininElementsURL)))
-                    return
-                }
+        guard let url2 = URL(string: remaininElementsURL ) else {
+            completion(.failure(.badURL(remaininElementsURL)))
+            return
+        }
         
         let request2 = URLRequest(url: url2)
         
         
-                NetworkHelper.shared.performDataTask(with: request2) { (result) in
-                    switch result {
-                    case .failure(let appError):
-                        completion(.failure(.networkClientError(appError)))
-                    case .success(let data) :
-                        do {
-                            let extraElements = try JSONDecoder().decode([ElementInfo].self, from: data)
-                            completion(.success(extraElements ))
-                        } catch {
-                            completion(.failure(.decodingError(error)))
-                        }
-                    }
+        NetworkHelper.shared.performDataTask(with: request2) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data) :
+                do {
+                    let extraElements = try JSONDecoder().decode([ElementInfo].self, from: data)
+                    completion(.success(extraElements ))
+                } catch {
+                    completion(.failure(.decodingError(error)))
                 }
+            }
+        }
         
     }
     
     static func fetchElements(completion: @escaping (Result<[ElementInfo], AppError>) ->()) {
         let elementEndpointURLString = "https://5c1d79abbc26950013fbcaa9.mockapi.io/api/v1/elements"
         
-     
+        
         
         guard let url = URL(string: elementEndpointURLString ) else {
             completion(.failure(.badURL(elementEndpointURLString)))
             return
         }
-
+        
         
         let request = URLRequest(url: url)
-      
+        
         
         NetworkHelper.shared.performDataTask(with: request) { (result) in
             switch result {
@@ -60,15 +60,15 @@ struct ElementAPIClient {
                     let elements = try JSONDecoder().decode([ElementInfo].self, from: data)
                     
                     
-                   // completion(.success(elements))
-                   fetchRemaining { (result) in
-                       switch result {
-                       case .failure(let appError):
-                           print("error: \(appError)")
-                       case .success(let elements2):
-                        completion(.success(elements + elements2))
-                       }
-                   }
+                    // completion(.success(elements))
+                    fetchRemaining { (result) in
+                        switch result {
+                        case .failure(let appError):
+                            print("error: \(appError)")
+                        case .success(let elements2):
+                            completion(.success(elements + elements2))
+                        }
+                    }
                     // TODO: before you write the completion call ther remainging elements
                     
                     
@@ -78,7 +78,7 @@ struct ElementAPIClient {
             }
         }
         
-
+        
     }
     
     static func postElement(element: ElementInfo, completion: @escaping (Result<Bool,AppError>) -> ()) {
@@ -93,7 +93,7 @@ struct ElementAPIClient {
             let data = try JSONEncoder().encode(element)
             var request = URLRequest(url: url)
             request.httpMethod = "POST"
-             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
             request.httpBody = data
             
             NetworkHelper.shared.performDataTask(with: request) { (result) in
@@ -118,7 +118,7 @@ struct ElementAPIClient {
             completion(.failure(.badURL(favoriteURLString)))
             return
         }
-         let request = URLRequest(url: url)
+        let request = URLRequest(url: url)
         
         NetworkHelper.shared.performDataTask(with: request) { (result) in
             switch result {
@@ -126,7 +126,7 @@ struct ElementAPIClient {
                 completion(.failure(.networkClientError(appError)))
             case .success(let data):
                 do {
-                   let favotritePodcasts = try JSONDecoder().decode([ElementInfo].self, from: data)
+                    let favotritePodcasts = try JSONDecoder().decode([ElementInfo].self, from: data)
                     completion(.success(favotritePodcasts))
                 } catch {
                     completion(.failure(.decodingError(error)))
